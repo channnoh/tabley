70d92129b50c7012e4d53fc5d849993a22527453
heechan <shgmlcks1004@naver.com>
2024-03-03 22:01:17 +0900

    Initial commit

---
+++ src/main/java/com/zerobase/tabley/TableyApplication.java
@@ -0,0 +1,13 @@
+package com.zerobase.tabley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TableyApplication.class, args);
+    }
+
+}
